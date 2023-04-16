08968c0fb3b4f2b5d0815ce97334c9c7ec9a3417
Marcela Herrera <marcelaalejandraherrera@yahoo.com.ar>
2023-04-16 20:11:02 -0300

    Se crea app Gym, 1ra version de HOME, carpeta templates

--- PIG_16_23318/settings.py
+++ PIG_16_23318/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gym',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'PIG_16_23318.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
