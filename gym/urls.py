61df6782b9b7efc3de1fdbd4651dfa29506c2539
marisa <ilmanamudi@gmail.com>
2023-04-30 22:10:59 -0300

    Se sube formulario de contacto

--- gym/urls.py
+++ gym/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path ('', views.home, name="home"),
+     path ('contacto/', views.contacto, name="contacto"),
+    path ('registrarse/', views.sing_up, name="sing_up"),
 ]
