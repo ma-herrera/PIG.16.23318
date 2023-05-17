b353ed11369beee6e9ae2e59f03fe2fde3f1b2ae
Marcela Herrera <marcelaalejandraherrera@yahoo.com.ar>
2023-05-17 18:02:59 -0300

    movemos modelos desde gym a nueva app administracion

--- PIG_16_23318/settings.py
+++ PIG_16_23318/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gym',
+    'administracion',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-AR'
 
 TIME_ZONE = 'UTC'
 
