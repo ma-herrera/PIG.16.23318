6dd8805997e0624fe9e06cd7352591b1daebf7c2
Marcela Herrera <marcelaalejandraherrera@yahoo.com.ar>
2023-05-22 13:11:32 -0300

    corregir filtro tipo de actividad

--- administracion/views.py
+++ administracion/views.py
@@ -20,7 +20,8 @@ def tipo_de_actividad_index(request):
     #queryset
     # tipo_de_actividad = TipoDeActividad.objects.filter(baja=False)
 
-    if request.GET['nombre']:
+    if request.GET:
+        # ['nombre']:
         snombre = request.GET['nombre']
         qs_tipos_de_actividad = TipoDeActividad.objects.filter(nombre=snombre)
     else:
