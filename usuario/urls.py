14c339a297d5251b49aecd102e48303ff5aebeef
marisa <ilmanamudi@gmail.com>
2023-05-23 16:11:08 -0300

    modificacion de usuario

--- usuario/urls.py
+++ usuario/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path ('cerrar_sesion', cerrar_sesion, name="cerrar_sesion"),
     path ('loguear', loguear, name="loguear"),
     path ('listado_usuarios/', ListadoUsuario.as_view(), name='listar_usuarios'),
-    path ('registrar_usuarios/',RegistrarUsuario.as_view(), name='registrar_usuarios'),
-    path ('eliminar/',EliminarUsuario.as_view(),name='eliminar_usuario'),
+    path ('registrar_usuarios/', RegistrarUsuario.as_view(), name='registrar_usuarios'),
+   # path ('eliminar/',EliminarUsuario.as_view(),name='eliminar_usuario'),
 
     
 
